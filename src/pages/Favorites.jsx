6fe0dc9910b462cec6831482eec615844296195f
bennadler <bennadlerr@gmail.com>
2023-02-22 15:38:51 +0200

    added memo for favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,14 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import { DataContext } from "../context/context";
 import RepoList from "../components/ReposList";
 
 const Favorites = () => {
   const { repoData, favorites } = useContext(DataContext);
-  return (
-    <RepoList
-      data={repoData.filter((repo) => (favorites[repo.id] ? true : false))}
-    />
-  );
+
+  const favoriteRepos = useMemo(() => {
+    return repoData.filter((repo) => (favorites[repo.id] ? true : false));
+  }, [favorites]);
+
+  return <RepoList data={favoriteRepos} />;
 };
 
 export default Favorites;
